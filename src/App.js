b68129907785194f34bc86767b62179bf9ded383
Jalen-Foreman <78430591+Jalen-Foreman@users.noreply.github.com>
2021-09-12 00:21:51 -0400

    Code notes for legibility

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
 import { Table, Button } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThumbsUp, faThumbsDown, faImage, faMoneyCheckAlt, faSearchDollar } from '@fortawesome/free-solid-svg-icons'
@@ -6,48 +6,35 @@ import { faThumbsUp, faThumbsDown, faImage, faMoneyCheckAlt, faSearchDollar } fr
 function App() {
   
   const [isLoading, setIsLoading] = useState(false)
-  const [invoices, setInvoices] = useState([
-	{
-		id: '100',
-		Vendor: 'Hankook',
-		Amount: '18,000',
-		Invoice: '1123',
-		Date: '08/21/2020',
-	},
-	{
-		id: '200',
-		Vendor: 'Hankook',
-		Amount: '18,000',
-		Invoice: '1123',
-		Date: '08/21/2020',
-	},
-	{
-		id: '300',
-		Vendor: 'Hankook',
-		Amount: '18,000',
-		Invoice: '1123',
-		Date: '08/21/2020',
-	},
-	{
-		id: '400',
-		Vendor: 'Hankook',
-		Amount: '18,000',
-		Invoice: '1123',
-		Date: '08/21/2020',
-	},
-])
+  const [invoices, setInvoices] = useState([])
+	// CONNECTING DATA FROM API TO STATE USING USE EFFECT AND ASYNC FUNCTIONS
+	useEffect(() => {
+		Mount();
+	}, [])
+
+	async function Mount() {
+		const response = await fetch(
+			'https://oj7ml8xy66.execute-api.us-east-2.amazonaws.com/Dev'
+		);
+		const body = await response.json();
+		await setInvoices(body);
+		setIsLoading(false);
+		}
 
   if (isLoading)
     return (<div>Loading...</div>)
 
+// UPDATING STATE EVERYTIME AN INVOICE IS REMOVED	
   const remove = (id) => {
-    const updatedInvoices = [...invoices].filter((i) => id !== i.id )
+    const updatedInvoices = invoices.filter((i) => id !== i.ID )
     setInvoices(updatedInvoices)
   }
-
+  
+  
+  // MAPPING THROUGH INVOICE DATA AND PLACING BUTTON LAYOUTS FOR INVOICES
   let allInvoices = invoices.map((invoice) => {
     return (
-			<tr key={invoice.id}>
+			<tr key={invoice.ID}>
 				<td>{invoice.Vendor}</td>
 				<td>{invoice.Amount}</td>
 				<td>{invoice.Invoice}</td>
@@ -55,7 +42,7 @@ function App() {
 				<td>
 					<Button
 						className='btn btn-lg btn-success'
-						onClick={() => remove(invoice.id)}>
+						onClick={() => remove(invoice.ID)}>
 						<FontAwesomeIcon icon={faThumbsUp} />
 						OK
 					</Button>
@@ -63,7 +50,7 @@ function App() {
 				<td>
 					<Button
 						className='btn btn-lg btn-danger'
-						onClick={() => remove(invoice.id)}>
+						onClick={() => remove(invoice.ID)}>
 						<FontAwesomeIcon icon={faThumbsDown} />
 						NOT OK
 					</Button>
@@ -71,7 +58,7 @@ function App() {
 				<td>
 					<Button
 						className='btn btn-lg btn-info'
-						onClick={() => remove(invoice.id)}>
+						onClick={() => remove(invoice.ID)}>
 						<FontAwesomeIcon icon={faMoneyCheckAlt} />
 						50%
 					</Button>
@@ -79,7 +66,7 @@ function App() {
 				<td>
 					<Button
 						className='btn btn-lg btn-warning'
-						onClick={() => remove(invoice.id)}>
+						onClick={() => remove(invoice.ID)}>
 						<FontAwesomeIcon icon={faSearchDollar} />
 						??
 					</Button>
@@ -87,7 +74,7 @@ function App() {
 				<td>
 					<Button
 						className='btn btn-lg btn-info'
-						onClick={() => remove(invoice.id)}>
+						onClick={() => remove(invoice.ID)}>
 						<FontAwesomeIcon icon={faImage} />
 						Image
 					</Button>
@@ -97,6 +84,7 @@ function App() {
   }
     )
 
+  // CREATING FRONTEND LAYOUT USING BOOTSTRAP
   return (
 		<div className='container border border-secondary rouded center'>
 			<div className='row'>
